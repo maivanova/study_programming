8255692bbe414ea9d035fb339540f49b097d0bf2
Maria Ivanova <146588454+maivanova@users.noreply.github.com>
2024-05-29 15:19:54 +0300

    комментарии к lab 2

--- geom.cpp
+++ geom.cpp
@@ -1,78 +1,74 @@
-#include "cmath"
-#include "iostream"
-
+#include <cmath>
+#include <iostream>
 using namespace std;
 
-
+// Определение класса Point для представления точек на плоскости
 class Point {
 public:
-    double x, y;
+    double x, y; // Координаты точки
 
+    // Конструктор для инициализации точки с заданными координатами или значениями по умолчанию (0, 0)
     Point(double x = 0, double y = 0);
 
+    // Метод для перемещения точки на заданные значения по оси x и y
     void move(double dx, double dy);
 };
 
+// Определение интерфейса для базовой фигуры
 class IBaseShape {
 public:
+    // Виртуальные методы для отрисовки, стирания, перемещения и вращения фигуры
     virtual void draw() = 0;
-
     virtual void erase() = 0;
-
     virtual void move(double dx, double dy) = 0;
-
     virtual void rotate(double angle) = 0;
 
+    // Виртуальный деструктор для правильного удаления объектов классов-наследников
     virtual ~IBaseShape() {}
 };
 
-
+// Определение класса Line, наследующего интерфейс базовой фигуры
 class Line : public IBaseShape {
 protected:
-    Point points[2];
+    Point points[2]; // Массив из двух точек, определяющих линию
+
 public:
+    // Конструктор для инициализации линии с двумя точками
     Line(Point p1, Point p2);
 
+    // Переопределение виртуальных методов базового класса
     void erase() override;
-
     void draw() override;
-
     void move(double dx, double dy) override;
-
     void rotate(double angle) override;
 };
 
+// Определение класса Square, наследующего интерфейс базовой фигуры
 class Square : public IBaseShape {
 public:
-    Point points[4];
-public:
+    Point points[4]; // Массив из четырех точек, определяющих квадрат
+
+    // Конструктор для инициализации квадрата с четырьмя точками
     Square(Point p1, Point p2, Point p3, Point p4);
 
+    // Переопределение виртуальных методов базового класса
     void draw() override;
-
     void erase() override;
-
     void move(double dx, double dy) override;
-
     void rotate(double angle);
-
 };
 
 class Rectangle : public IBaseShape {
 public:
-    Point points[4];
+    Point points[4]; // Массив для хранения четырех точек
 public:
     Rectangle(Point p1, Point p2, Point p3, Point p4);
 
-    void erase() override;
-
-    void draw() override;
-
-    void move(double dx, double dy) override;
-
-    void rotate(double angle) override;
+    void erase() override; // Метод для стирания
+    void draw() override; // Метод для отрисовки
+    void move(double dx, double dy) override; // Метод для перемещения
+    void rotate(double angle) override; // Метод для поворота
 };
-
 class Parallelogram : public IBaseShape {
 public:
     Point points[4];
@@ -105,112 +101,88 @@ public:
 
 Point::Point(double x, double y) {
     this->x = x;
-    this->y = y;
+    this->y = y;  // Инициализация координат точки
 }
 
 void Point::move(double dx, double dy) {
     x += dx;
-    y += dy;
+    y += dy; // Изменение координат точки при перемещении
 }
 
 Line::Line(Point p1, Point p2) {
-    points[0] = p1;
-    points[1] = p2;
+    points[0] = p1;    points[1] = p2; // Установка начальной и конечной точек линии
 }
 
 void Line::draw() {
-    cout << "Line was drew" << endl;
+    cout << "Line was drew" << endl; // Вывод сообщения о рисовании
 }
 
 void Line::erase() {
-    cout << "Line was erased" << endl;
+    cout << "Line was erased" << endl; // Вывод сообщения об стирании
 }
 
 void Line::move(double dx, double dy) {
-    for (auto &point: points) {
-        point.move(dx, dy);
-    }
+    for (auto &point: points) {        point.move(dx, dy);    } // Перемещение каждой точки линии
 }
 
-const double M_PI = 3.14;
+const double M_PI = 3.14; // Значение числа Пи
+
 void Line::rotate(double angle) {
-    double angleR = angle * M_PI / 180;
-    double centerX, centerY;
+    double angleR = angle * M_PI / 180; // Перевод угла в радианы
+    double centerX, centerY; // Координаты центра вращения
+    for (auto &point: points) {        centerX += point.x;        centerY += point.y;    } // Вычисление центра вращения
+    centerX /= 2;    centerY /= 2; // Нахождение средних координат
     for (auto &point: points) {
-        centerX += point.x;
-        centerY += point.y;
-    }
-    centerX /= 2;
-    centerY /= 2;
-    for (auto &point: points) {
-        double oldX = point.x;
-        point.x = (point.x - centerX) * cos(angleR) - (point.y - centerY) * sin(angleR) + centerX;
-        point.y = (oldX - centerX) * sin(angleR) + (point.y - centerY) * cos(angleR) + centerX;
-        cout << "(" << point.x << ", " << point.y << ")" << endl;
-    }
+        double oldX = point.x;        point.x = (point.x - centerX) * cos(angleR) - (point.y - centerY) * sin(angleR) + centerX;
+        point.y = (oldX - centerX) * sin(angleR) + (point.y - centerY) * cos(angleR) + centerY;    } // Поворот каждой точки линии и вывод координат
 }
 
 Square::Square(Point p1, Point p2, Point p3, Point p4) {
-    points[0] = p1;
-    points[1] = p2;
-    points[2] = p3;
-    points[3] = p4;
+    points[0] = p1;    points[1] = p2;
+    points[2] = p3;    points[3] = p4; // Установка четырех точек квадрата
 }
-
 void Square::draw() {
-    cout << "The square is drew" << endl;
+    cout << "The square is drew" << endl; // Вывод сообщения о рисовании квадрата
 }
 
 void Square::erase() {
-    cout << "The square is erased" << endl;
+    cout << "The square is erased" << endl; // Вывод сообщения об стирании квадрата
 }
 
 void Square::move(double dx, double dy) {
-    for (auto &point: points) {
-        point.move(dx, dy);
-        cout << "(" << point.x << ", " << point.y << ")" << endl;
-    }
+    for (auto &point: points) {        point.move(dx, dy);        cout << "(" << point.x << ", " << point.y << ")" << endl;    } // Перемещение каждой точки квадрата и вывод координат
 }
 
 void Square::rotate(double angle) {
-    double angleR = angle * M_PI / 180;
-    double centerX, centerY;
-    centerX = (points[0].x + points[2].x) / 2;
-    centerY = (points[0].y + points[2].y) / 2;
-    double sinA = sin(angleR);
-    double cosA = cos(angleR);
+    double angleR = angle * M_PI / 180;    double centerX, centerY;
+    centerX = (points[0].x + points[2].x) / 2;    centerY = (points[0].y + points[2].y) / 2; // Вычисление центра квадрата
+    double sinA = sin(angleR);    double cosA = cos(angleR); // Синус и косинус угла
     for (auto &point: points) {
-        double oldX = point.x;
-        double oldY = point.y;
-        point.x = (oldX - centerX) * cosA - (oldY - centerY) * sinA + centerX;
-        point.y = (oldX - centerX) * sinA + (oldY - centerY) * cosA + centerY;
-        cout << "(" << point.x << ", " << point.y << ")" << endl;
-    }
+        double oldX = point.x;        double oldY = point.y; // Сохранение старых координат
+        point.x = (oldX - centerX) * cosA - (oldY - centerY) * sinA + centerX; // Поворот точки вокруг центра
+        point.y = (oldX - centerX) * sinA + (oldY - centerY) * cosA + centerY;        cout << "(" << point.x << ", " << point.y << ")" << endl;
+    } // Поворот каждой точки квадрата и вывод координат
 }
-
 Rectangle::Rectangle(Point p1, Point p2, Point p3, Point p4) {
-    points[0] = p1;
-    points[1] = p2;
-    points[2] = p3;
-    points[3] = p4;
+    points[0] = p1;    points[1] = p2;
+    points[2] = p3;    points[3] = p4; // Установка четырех точек прямоугольника
 }
 
 void Rectangle::draw() {
-    cout << "Rectangle was drew" << endl;
+    cout << "Rectangle was drew" << endl; // Вывод сообщения о рисовании прямоугольника
 }
 
 void Rectangle::erase() {
-    cout << "Rectangle was erased" << endl;
+    cout << "Rectangle was erased" << endl; // Вывод сообщения об стирании прямоугольника
 }
 
 void Rectangle::move(double dx, double dy) {
-    for (auto &point: points) {
-        point.move(dx, dy);
-        cout << "(" << point.x << ", " << point.y << ")" << endl;
-    }
-
+    for (auto &point: points) {        point.move(dx, dy);        cout << "(" << point.x << ", " << point.y << ")" << endl;    } // Перемещение каждой точки прямоугольника и вывод координат
 }
 
+// Определение метода для поворота прямоугольника на заданный угол
+// Пересчет координат точек прямоугольника после поворота вокруг центра
+// Вывод новых координат точек и центра прямоугольника
 void Rectangle::rotate(double angle) {
     double angleR = angle * M_PI / 180;
     double centerX, centerY;
@@ -229,6 +201,7 @@ void Rectangle::rotate(double angle) {
     cout << centerY;
 }
 
+// Определение конструктора для параллелограмма на основе четырех точек
 Parallelogram::Parallelogram(Point p1, Point p2, Point p3, Point p4) {
     points[0] = p1;
     points[1] = p2;
@@ -236,59 +209,27 @@ Parallelogram::Parallelogram(Point p1, Point p2, Point p3, Point p4) {
     points[3] = p4;
 }
 
+// Вывод сообщения о нарисованном параллелограмме
 void Parallelogram::draw() {
     cout << "Parallelogram was drew" << endl;
 }
 
+// Вывод сообщения об удаленном параллелограмме
 void Parallelogram::erase() {
     cout << "Parallelogram was erased" << endl;
 }
 
+// Определение метода для перемещения параллелограмма на вектор (dx, dy)
+// И вывод новых координат точек
 void Parallelogram::move(double dx, double dy) {
     for (auto &point: points) {
         point.move(dx, dy);
         cout << "(" << point.x << ", " << point.y << ")" << endl;
     }
-
-}
-void Parallelogram::rotate(double angle) {
-    double angleR = angle * M_PI / 180;
-    double centerX, centerY;
-    centerX = (points[0].x + points[2].x) / 2;
-    centerY = (points[0].y + points[2].y) / 2;
-    double sinA = sin(angleR);
-    double cosA = cos(angleR);
-    for (auto &point: points) {
-        double oldX = point.x;
-        double oldY = point.y;
-        point.x = (oldX - centerX) * cosA - (oldY - centerY) * sinA + centerX;
-        point.y = (oldX - centerX) * sinA + (oldY - centerY) * cosA + centerY;
-        cout << "(" << point.x << ", " << point.y << ")" << endl;
-    }
 }
 
-Rhombus::Rhombus(Point p1, Point p2, Point p3, Point p4) {
-    points[0] = p1;
-    points[1] = p2;
-    points[2] = p3;
-    points[3] = p4;
-}
-
-void Rhombus::draw() {
-    cout << "Parallelogram was drew" << endl;
-}
-
-void Rhombus::erase() {
-    cout << "Parallelogram was erased" << endl;
-}
-
-void Rhombus::move(double dx, double dy) {
-    for (auto &point: points) {
-        point.move(dx, dy);
-        cout << "(" << point.x << ", " << point.y << ")" << endl;
-    }
-
-}
+// Определение метода для поворота ромба на заданный угол
+// Аналогично реализации поворота прямоугольника
 void Rhombus::rotate(double angle) {
     double angleR = angle * M_PI / 180;
     double centerX, centerY;
@@ -301,6 +242,33 @@ void Rhombus::rotate(double angle) {
         double oldY = point.y;
         point.x = (oldX - centerX) * cosA - (oldY - centerY) * sinA + centerX;
         point.y = (oldX - centerX) * sinA + (oldY - centerY) * cosA + centerY;
+        cout << "(" << point.x << ", " << point.y << ")" << endl;
+    }
+}
+
+// Определение конструктора для ромба на основе четырех точек
+Rhombus::Rhombus(Point p1, Point p2, Point p3, Point p4) {
+    points[0] = p1;
+    points[1] = p2;
+    points[2] = p3;
+    points[3] = p4;
+}
+
+// Вывод сообщения о нарисованном ромбе
+void Rhombus::draw() {
+    cout << "Parallelogram was drew" << endl;
+}
+
+// Вывод сообщения об удаленном ромбе
+void Rhombus::erase() {
+    cout << "Parallelogram was erased" << endl;
+}
+
+// Метод для перемещения ромба на вектор (dx, dy)
+// И вывод новых координат точек
+void Rhombus::move(double dx, double dy) {
+    for (auto &point: points) {
+        point.move(dx, dy);
         cout << "(" << point.x << ", " << point.y << ")" << endl;
     }
 }
@@ -325,5 +293,4 @@ int main() {
     //Rhombus.rotate(90);
 
 }
-
 
